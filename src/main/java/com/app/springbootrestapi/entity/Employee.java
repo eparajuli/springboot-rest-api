5aef1d55a68b979c6d4a661777ea1064b00dabf9
enjalparajuli <enjalparajuli@gmail.com>
2019-06-29 16:04:02 -0600

    Update application.properties to connect to my database

--- src/main/java/com/app/springbootrestapi/entity/Employee.java
+++ src/main/java/com/app/springbootrestapi/entity/Employee.java
@@ -7,13 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
 @Table(name = "employee") //this is how the name should appear in database table
-@Setter
-@Getter
+//@Setter
+//@Getter
 public class Employee {
 	
 	@Id //primary ID
@@ -21,11 +18,33 @@ public class Employee {
 	@GeneratedValue(strategy=GenerationType.AUTO) //defines how the id should be generated. Modify it as per your requirements
 	private Long employeeId;
 	
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+
 	@Column(name = "employee_name")
 	private String employeeName;
 	
 	@Column(name = "salary")
 	private Long salary;
-	
-
 }
