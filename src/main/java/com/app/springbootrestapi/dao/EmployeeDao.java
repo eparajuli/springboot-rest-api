39b0b357f5bf9cb2b8022d6519b89352f7f524ea
enjalparajuli <enjalparajuli@gmail.com>
2019-06-29 10:02:05 -0600

    Added employee controller

--- src/main/java/com/app/springbootrestapi/dao/EmployeeDao.java
+++ src/main/java/com/app/springbootrestapi/dao/EmployeeDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.springbootrestapi.entity.Employee;
 
+//used long because our Employee ID is of type long
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
 	
 }
